2afa05d82564856581192f37b05f670120f6a56b
buket.culfaoglu <buket.culfaoglu@prestijyazilim.com.tr>
2021-10-22 16:51:04 +0300

    fetching and rendering posts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 // export default () =>
 // {
@@ -12,6 +13,10 @@ const App = () =>
         <div className="container">
             <h1>Create Post</h1>
             <PostCreate />
+
+            <hr />
+            <h1>List Post</h1>
+            <PostList />
         </div>
     );
 
